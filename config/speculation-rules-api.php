5b366d6b775442336601c1d6534fd7f7e3315ff6
authanram <authanram@users.noreply.github.com>
2024-08-09 16:21:22 +0000

    Fix styling

--- config/speculation-rules-api.php
+++ config/speculation-rules-api.php
@@ -5,30 +5,30 @@ return [
     'default_eagerness' => 'moderate',
 
     'prerender' => [
-//        [
-//            'source' => 'list',
-//            'urls' => ['page-1', 'page-2'],
-//            'eagerness' => 'moderate',
-//        ],
-//        // parameter usage example
-//        [
-//            [
-//                'href_matches' => '/page-3/*'
-//            ],
-//            [
-//                'not' => [
-//                    'href_matches' => '/page-3/*/*'
-//                ]
-//            ],
-//        ],
+        //        [
+        //            'source' => 'list',
+        //            'urls' => ['page-1', 'page-2'],
+        //            'eagerness' => 'moderate',
+        //        ],
+        //        // parameter usage example
+        //        [
+        //            [
+        //                'href_matches' => '/page-3/*'
+        //            ],
+        //            [
+        //                'not' => [
+        //                    'href_matches' => '/page-3/*/*'
+        //                ]
+        //            ],
+        //        ],
     ],
 
     'prefetch' => [
-//        [
-//            'urls' => ['page-4'],
-//            'referrer_policy' => 'no-referrer',
-//            'eagerness' => 'moderate',
-//        ],
+        //        [
+        //            'urls' => ['page-4'],
+        //            'referrer_policy' => 'no-referrer',
+        //            'eagerness' => 'moderate',
+        //        ],
     ],
 
 ];
