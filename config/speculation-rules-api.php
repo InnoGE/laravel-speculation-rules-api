357c5e84d5febafb2e334a31035cef24ed8b30c2
Daniel Seuffer <authanram@gmail.com>
2024-08-09 18:20:33 +0200

    Add configuration examples

--- config/speculation-rules-api.php
+++ config/speculation-rules-api.php
@@ -1,14 +1,34 @@
 <?php
 
 return [
+
+    'default_eagerness' => 'moderate',
+
     'prerender' => [
-        [
-            //
-        ],
+//        [
+//            'source' => 'list',
+//            'urls' => ['page-1', 'page-2'],
+//            'eagerness' => 'moderate',
+//        ],
+//        // parameter usage example
+//        [
+//            [
+//                'href_matches' => '/page-3/*'
+//            ],
+//            [
+//                'not' => [
+//                    'href_matches' => '/page-3/*/*'
+//                ]
+//            ],
+//        ],
     ],
+
     'prefetch' => [
-        [
-            //
-        ],
+//        [
+//            'urls' => ['page-4'],
+//            'referrer_policy' => 'no-referrer',
+//            'eagerness' => 'moderate',
+//        ],
     ],
+
 ];
